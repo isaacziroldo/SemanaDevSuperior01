451101e5da7d963f43209b1041740317af023da8
Isaac Ziroldo <isaac.ziroldo@gmail.com>
2020-09-19 11:03:42 -0300

    Criando Rotas de Navegação

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
 import './App.css';
+import Routes from './Routes';
 
 function App() {
   return (
-    <Header />
+    <Routes />
   );
 }
 
