6781f7195c977c2f23ea710839ba963637712f25
Isaac Ziroldo <isaac.ziroldo@gmail.com>
2020-09-19 14:06:24 -0300

    FInalizando Gráficos

--- front-web/src/pages/Charts/types.ts
+++ front-web/src/pages/Charts/types.ts
@@ -1,12 +1,7 @@
 import { Platform } from "../Records/types";
 
 export type Game = {
-    id: number
-    title: string
-    platform: Platform
-}
-
-export type ChartItem = {
-    x:string
-    y: number
+    id: number;
+    title: string;
+    platform: Platform;
 }
