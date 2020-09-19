209db676ee65539de36b7959adebdd413f53cf78
Isaac Ziroldo <isaac.ziroldo@gmail.com>
2020-09-19 10:47:31 -0300

    Adcionando Component Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Header />
   );
 }
 
